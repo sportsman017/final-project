64b1e3ac9266a7467dbfef7a8a077f27a52849eb
sportsman017 <sportsman0170@gmail.com>
2025-06-19 01:02:14 -0700

    Add files via upload

--- Q4/frequency based policy/Caches.py
+++ Q4/frequency based policy/Caches.py
@@ -80,7 +80,7 @@ class L3Cache(Cache):
     mshrs = 32
     tgts_per_mshr = 24
     write_buffers = 16
-    replacement_policy = Param.BaseReplacementPolicy(RRIPRP(),"Replacement policy")
+    replacement_policy = Param.BaseReplacementPolicy(LFURP(),"Replacement policy")
 
 class IOCache(Cache):
     assoc = 8
